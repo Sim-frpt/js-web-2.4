8ad2b04b62800669896d3c332255f21c4109d9a5
Simon <simon.fraipont@gmail.com>
2019-02-12 15:05:30 +0100

    début 2.4, display modale fonctionnelle

--- script.js
+++ script.js
@@ -9,4 +9,84 @@ import 'bootstrap';
   Put the JavaScript code you want below.
 */
 
-console.log("Hey look in your browser console. It works!");
+import axios from "axios";
+import {markdown} from "markdown";
+
+
+const getCharacters = async() => {
+  try {
+    const response = axios.get("https://character-database.becode.xyz/characters");
+    console.log(response);
+    return response;
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+const displayCharacters = async() => {
+  let charObject = await getCharacters();
+  let charArray = charObject.data;
+  let myDiv = document.querySelector("#container");
+
+  for(let i = 0; i < charArray.length; i++) {
+    let card = document.createElement("div");
+    let name = document.createElement("h5");
+    let shortDescription = document.createElement("p");
+    let image = new Image(100, 100);
+
+    card.setAttribute("class", "card");
+    name.setAttribute("class", "name");
+    name.setAttribute("data-toggle", "modal");
+    name.setAttribute("data-target", "#displayModal");
+    name.addEventListener("click", function () {
+      displayModalContent(charArray[i]);
+    });
+    shortDescription.setAttribute("class", "shortDescript");
+    image.setAttribute("class", "image");
+
+    name.innerText = charArray[i].name;
+    shortDescription.innerText = charArray[i].shortDescription;
+
+    myDiv.appendChild(card);
+    card.appendChild(name);
+    card.appendChild(shortDescription);
+
+    if(charArray[i].image) {
+      image.src = "data:image/jpeg;base64," + charArray[i].image;
+      card.appendChild(image);
+    }
+  }
+};
+
+//Create the content of the display modal
+const displayModalContent = (arrayElement) => {
+  let myModal = document.querySelector("#displayModal");
+  let modalTitle = document.querySelector("#characterCard");
+  let modalContent = document.querySelector("#modalContent");
+  modalContent.innerHTML = "";
+  let name = document.createElement("h5");
+  let shortDescription = document.createElement("p");
+  let image = new Image(100, 100);
+  let description = document.createElement("p");
+
+  name.setAttribute("class", "modalName");
+  shortDescription.setAttribute("class", "modalShortDescript");
+  image.setAttribute("class", "modalImage");
+  description.setAttribute("class", "modalDescription");
+  modalTitle.innerText = "Character Card";
+  name.innerText = arrayElement.name;
+  shortDescription.innerText = arrayElement.shortDescription;
+  description.innerText = arrayElement.description;
+
+
+  if(arrayElement.image) {
+    image.src = "data:image/jpeg;base64," + arrayElement.image;
+    modalContent.appendChild(image);
+  }
+  modalContent.appendChild(name);
+  modalContent.appendChild(shortDescription);
+  modalContent.appendChild(description);
+  modalContent.appendChild(image);
+};
+
+displayCharacters();
