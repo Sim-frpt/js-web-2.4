0fedbbf40c81e2e2521666f0e24d7fe8bfe50840
Simon <simon.fraipont@gmail.com>
2019-02-12 17:02:50 +0100

    début post request marche

--- script.js
+++ script.js
@@ -15,17 +15,16 @@ import {markdown} from "markdown";
 
 const getCharacters = async() => {
   try {
-    const response = axios.get("https://character-database.becode.xyz/characters");
-    console.log(response);
-    return response;
+    const response = await axios.get("https://character-database.becode.xyz/characters");
+    console.log(response.data);
+    return response.data;
   } catch(error) {
     console.log(error);
   }
 };
 
 const displayCharacters = async() => {
-  let charObject = await getCharacters();
-  let charArray = charObject.data;
+  let charArray = await getCharacters();
   let myDiv = document.querySelector("#container");
 
   for(let i = 0; i < charArray.length; i++) {
@@ -62,7 +61,7 @@ const displayCharacters = async() => {
 const displayModalContent = (arrayElement) => {
   let myModal = document.querySelector("#displayModal");
   let modalTitle = document.querySelector("#characterCard");
-  let modalContent = document.querySelector("#modalContent");
+  let modalContent = document.querySelector("#displayContent");
   modalContent.innerHTML = "";
   let name = document.createElement("h5");
   let shortDescription = document.createElement("p");
@@ -76,17 +75,44 @@ const displayModalContent = (arrayElement) => {
   modalTitle.innerText = "Character Card";
   name.innerText = arrayElement.name;
   shortDescription.innerText = arrayElement.shortDescription;
-  description.innerText = arrayElement.description;
+  description.innerHTML = markdown.toHTML(arrayElement.description);
 
 
   if(arrayElement.image) {
     image.src = "data:image/jpeg;base64," + arrayElement.image;
-    modalContent.appendChild(image);
+    displayContent.appendChild(image);
   }
-  modalContent.appendChild(name);
-  modalContent.appendChild(shortDescription);
-  modalContent.appendChild(description);
-  modalContent.appendChild(image);
+  displayContent.appendChild(name);
+  displayContent.appendChild(shortDescription);
+  displayContent.appendChild(description);
+  displayContent.appendChild(image);
+};
+
+
+//Create a new character modal
+const PostcreatorModal = async () => {
+  let form = document.querySelector("#creationForm");
+  let creationModal = document.querySelector("#charCreationModal");
+  let creaTion = document.querySelector("#characterCard");
+  let characterName = document.querySelector("#addName").value;
+  let charShortDescription = document.querySelector("#addShortDescription").value;
+  let charDescription = document.querySelector("#addDescription").value;
+
+  let newCharacter = {
+    name: characterName,
+    shortDescription: charShortDescription,
+    description: charDescription
+  }
+
+  document.querySelector("#addName").value = "";
+  document.querySelector("#addShortDescription").value = "";
+  document.querySelector("#addDescription").value = "";
+  await axios.post("https://character-database.becode.xyz/characters", newCharacter);
+  window.location.reload();
 };
 
 displayCharacters();
+
+document.querySelector("#creationButton").addEventListener("click", function() {
+  PostcreatorModal();
+});
